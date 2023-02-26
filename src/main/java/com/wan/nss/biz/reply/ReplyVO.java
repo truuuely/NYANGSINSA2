5763b225163821f6fbf8fa9f40002c1287f69feb
truuuely <kmyc7030@gmail.com>
2023-02-27 02:32:26 +0900

    Update ReplyVO.java
    댓글 작성자 ID 추가

--- src/main/java/com/wan/nss/biz/reply/ReplyVO.java
+++ src/main/java/com/wan/nss/biz/reply/ReplyVO.java
@@ -3,12 +3,15 @@ package com.wan.nss.biz.reply;
 public class ReplyVO {
 	private int replyNum; // 댓글 번호
 	private int boardNum; // 글 번호
-	private int memberNum; // 댓글 작성자
+	private int memberNum; // 댓글 작성자 번호
 	private String replyDate; // 댓글 작성일
 	private String replyContent; // 댓글 내용
 	private int replyStatus; // 댓글 상태
 	private int replyStep; // 댓글의 계층
 	private int parentNum; // 부모 댓글의 댓글 번호
+
+	private String memberId; // 댓글 작성자 id
+	private String parentId; // 부모 댓글 작성자 id
 
 	public int getReplyNum() {
 		return replyNum;
@@ -74,10 +77,26 @@ public class ReplyVO {
 		this.parentNum = parentNum;
 	}
 
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+
 	@Override
 	public String toString() {
 		return "ReplyVO [replyNum=" + replyNum + ", boardNum=" + boardNum + ", memberNum=" + memberNum + ", replyDate="
 				+ replyDate + ", replyContent=" + replyContent + ", replyStatus=" + replyStatus + ", replyStep="
-				+ replyStep + ", parentNum=" + parentNum + "]";
+				+ replyStep + ", parentNum=" + parentNum + ", memberId=" + memberId + ", parentId=" + parentId + "]";
 	}
 }
