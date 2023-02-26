7de02bef1a6b8c54a2c9fd63f2e7ec0198f30ab7
truuuely <kmyc7030@gmail.com>
2023-02-27 01:42:51 +0900

    Update ReportVO.java
    신고VO 신고당한 회원 ID 멤버변수 추가

--- src/main/java/com/wan/nss/biz/report/ReportVO.java
+++ src/main/java/com/wan/nss/biz/report/ReportVO.java
@@ -8,53 +8,79 @@ public class ReportVO {
 	private String reportDate; // 신고 일자
 	private String reportContent; // 신고 내용
 	private int status; // 신고 상태 (1: 정상, 2: 신고, 3: 삭제)
-	
+
+//	vo에만 존재하는 멤버 변수
+	private String memberId; // 신고 당한 회원의 ID
+
 	public int getReportNum() {
 		return reportNum;
 	}
+
 	public void setReportNum(int reportNum) {
 		this.reportNum = reportNum;
 	}
+
 	public int getTargetNum() {
 		return targetNum;
 	}
+
 	public void setTargetNum(int targetNum) {
 		this.targetNum = targetNum;
 	}
+
 	public int getReportStep() {
 		return reportStep;
 	}
+
 	public void setReportStep(int reportStep) {
 		this.reportStep = reportStep;
 	}
+
 	public int getMemberNum() {
 		return memberNum;
 	}
+
 	public void setMemberNum(int memberNum) {
 		this.memberNum = memberNum;
 	}
+
 	public String getReportDate() {
 		return reportDate;
 	}
+
 	public void setReportDate(String reportDate) {
 		this.reportDate = reportDate;
 	}
+
 	public String getReportContent() {
 		return reportContent;
 	}
+
 	public void setReportContent(String reportContent) {
 		this.reportContent = reportContent;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
 	@Override
 	public String toString() {
 		return "ReportVO [reportNum=" + reportNum + ", targetNum=" + targetNum + ", reportStep=" + reportStep
 				+ ", memberNum=" + memberNum + ", reportDate=" + reportDate + ", reportContent=" + reportContent
-				+ ", status=" + status + "]";
+				+ ", status=" + status + ", memberId=" + memberId + "]";
 	}
+
 }
