65ee11650edad4063369e6c8744f6e1445b5b24d
woojh7142 <woojh7142@gmail.com>
2023-02-28 16:31:55 +0900

    Product Ctrl 수정, 에러로그 추가, 나머지 로그 수정

--- src/main/java/com/wan/nss/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/wan/nss/biz/common/AfterReturningAdvice.java
@@ -1,4 +1,4 @@
-package com.wan.nss.common;
+package com.wan.nss.biz.common;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class AfterReturningAdvice {
 	@AfterReturning(pointcut="PointcutCommon.aPointcut()", returning="obj")
 	public void printLogARA(JoinPoint jp, Object obj) {
+		System.out.println("[Ctrl() 실행 후 로그]");
 		
 		System.out.println(jp.getSignature().getName()+"()이 수행 완료되었습니다.");
 		System.out.println(jp.getArgs()[0]+"가 인자로 쓰였습니다.");
---
+++ src/main/java/com/wan/nss/biz/common/AfterThrowingAdvice.java
@@ -0,0 +1,16 @@
+package com.wan.nss.biz.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
+
+@Service
+@Aspect
+public class AfterThrowingAdvice {
+	@AfterThrowing(pointcut="PointcutCommon.aPointcut()", throwing="excepObj")
+	public void printLogATA(JoinPoint jp, Exception excepObj) {
+	
+		System.out.println("[Ctrl 예외 로그] "+jp.getSignature().getName()+" 비즈니스 메서드를 수행하던중 "+excepObj.getMessage()+" 예외발생");
+	}
+}
