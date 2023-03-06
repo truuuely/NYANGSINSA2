9e4c03a550c54e1e557c1c7a4ed66580e7595956
truuuely <kmyc7030@gmail.com>
2023-03-06 16:51:00 +0900

    Update ReportDAO.java : 신고자 및 신고당한자 아이디 추가

--- src/main/java/com/wan/nss/biz/report/ReportDAO.java
+++ src/main/java/com/wan/nss/biz/report/ReportDAO.java
@@ -15,22 +15,20 @@ public class ReportDAO {
 	private JdbcTemplate jdbcTemplate;
 
 	/*
-	 * C : 게시글, 댓글/대댓글 신고. TARGET_NO 에는 게시글 PK 혹은 댓글 PK가 들어가야 함.
-	 * 	 RP_STEP = 1(글), 2(댓글), 3(대댓글)
+	 * C : 게시글, 댓글/대댓글 신고. TARGET_NO 에는 게시글 PK 혹은 댓글 PK가 들어가야 함. RP_STEP = 1(글),
+	 * 2(댓글), 3(대댓글)
 	 */
 	private final String INSERT = "INSERT INTO REPORT (TARGET_NO, RP_STEP, M_NO, RP_CONTENT) VALUES (?, ?, (SELECT M_NO FROM MEMBER WHERE M_ID = ?), ?)";
 
-	// R : Report 전체 보기 (삭제된 신고글 제외)
-	private final String SELECT_ALL = "SELECT * FROM REPORT WHERE STATUS != 3 ORDER BY RP_NO DESC";
+	// R : Report 전체 보기
+	private final String SELECT_ALL = "SELECT r.*, m1.M_ID AS M_ID, m2.M_ID AS REPORTER_ID FROM REPORT r INNER JOIN `MEMBER` m1 ON r.M_NO = m1.M_NO INNER JOIN `MEMBER` m2 ON r.RP_M_NO  = m2.M_NO ORDER BY RP_NO DESC";
 
 	// R : Report 상세보기
-	private final String SELECT_ONE = "SELECT * FROM REPORT WHERE RP_NO = ?";
+	// ? : rpNum
+	private final String SELECT_ONE = "SELECT r.*, m1.M_ID AS M_ID, m2.M_ID AS REPORTER_ID FROM REPORT r INNER JOIN `MEMBER` m1 ON r.M_NO = m1.M_NO INNER JOIN `MEMBER` m2 ON r.RP_M_NO  = m2.M_NO WHERE RP_NO = ?";
 
-	// U : Report 신고 취소(정상)
-	private final String UPDATE = "UPDATE REPORT SET STATUS = 1 WHERE RP_NO = ?";
-
-	// D : Report 삭제 처리
-	private final String DELETE = "UPDATE REPORT SET STATUS = 3 WHERE RP_NO = ?";
+	// U : Report (처리 전 : 1, 처리 후 : 2)
+	private final String UPDATE = "UPDATE REPORT SET STATUS = ? WHERE RP_NO = ?";
 
 	public boolean insert(ReportVO vo) {
 		if (jdbcTemplate.update(INSERT, vo.getTargetNum(), vo.getReportStep(), vo.getUserId(),
@@ -47,20 +45,15 @@ public class ReportDAO {
 	public ReportVO selectOne(ReportVO vo) {
 		return jdbcTemplate.queryForObject(SELECT_ONE, new ReportRowMapper(), vo.getReportNum());
 	}
-	
+
 	public boolean update(ReportVO vo) {
-		if (jdbcTemplate.update(UPDATE, vo.getReportNum()) < 1) {
+		// status => 처리 전 : 1, 처리 후 : 2
+		if (jdbcTemplate.update(UPDATE, vo.getReportStat(),vo.getReportNum()) < 1) {
 			return false;
 		}
 		return true;
 	}
 
-	public boolean delete(ReportVO vo) {
-		if (jdbcTemplate.update(DELETE, vo.getReportNum()) < 1) {
-			return false;
-		}
-		return true;
-	}
 }
 
 class ReportRowMapper implements RowMapper<ReportVO> {
@@ -71,9 +64,13 @@ class ReportRowMapper implements RowMapper<ReportVO> {
 		data.setTargetNum(rs.getInt("TARGET_NO"));
 		data.setReportStep(rs.getInt("RP_STEP"));
 		data.setUserNum(rs.getInt("M_NO"));
+		data.setReporterNum(rs.getInt("RP_M_NO"));
 		data.setReportDate(rs.getString("RP_DATE"));
 		data.setReportContent(rs.getString("RP_CONTENT"));
 		data.setReportStat(rs.getInt("STATUS"));
+
+		data.setUserId(rs.getString("M_ID"));
+		data.setReporterId(rs.getString("REPORTER_ID"));
 		return data;
 	}
 
