21c484c3d0a72661a9c85e82fdf9e3090fe08875
KIM SEUNG-UK <117734687+seungukkim123@users.noreply.github.com>
2023-03-09 17:46:29 +0900

    ckeditor
    이미지 업로드 완료, 이미지 파일 저장 보완 필

---
+++ src/main/java/com/wan/nss/controller/CkeditorFileUploadController.java
@@ -0,0 +1,100 @@
+package com.wan.nss.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.google.gson.JsonObject;
+
+@Controller
+@RequestMapping("/adm/")
+public class CkeditorFileUploadController {
+
+	@RequestMapping("/fileupload.do")
+	@ResponseBody
+	public String fileUpload(HttpServletRequest request, HttpServletResponse response,
+			MultipartHttpServletRequest multiFile) throws IOException {
+	
+		//Json 객체 생성
+		JsonObject json = new JsonObject();
+		// Json 객체를 출력하기 위해 PrintWriter 생성
+		PrintWriter printWriter = null;
+		OutputStream out = null;
+		//파일을 가져오기 위해 MultipartHttpServletRequest 의 getFile 메서드 사용
+		MultipartFile file = multiFile.getFile("upload");
+		//파일이 비어있지 않고(비어 있다면 null 반환)
+		if (file != null) {
+			// 파일 사이즈가 0보다 크고, 파일이름이 공백이 아닐때
+			if (file.getSize() > 0 && StringUtils.isNotBlank(file.getName())) {
+				if (file.getContentType().toLowerCase().startsWith("image/")) {
+
+					try {
+						//파일 이름 설정
+						String fileName = file.getName();
+						System.out.println("fileName: "+fileName);
+						//바이트 타입설정
+						byte[] bytes;
+						//파일을 바이트 타입으로 변경
+						bytes = file.getBytes();
+						//파일이 실제로 저장되는 경로 
+						String uploadPath = request.getServletContext().getRealPath("/images/boardimages/");
+						System.out.println("uploadPath: "+uploadPath);
+						//저장되는 파일에 경로 설정
+						File uploadFile = new File(uploadPath);
+						System.out.println("uploadFile: "+uploadFile);
+						if (!uploadFile.exists()) {
+							System.out.println("없는 경로이므로 파일 생성");
+							uploadFile.mkdirs();
+							System.out.println("없는 경로이므로 파일 생성 완료");
+						}
+						//파일이름을 랜덤하게 생성
+						fileName = UUID.randomUUID().toString();
+						//업로드 경로 + 파일이름을 줘서 데이터를 서버에 전송
+						uploadPath = uploadPath + "/" + fileName;
+						// 이미지 파일 생성
+						out = new FileOutputStream(new File(uploadPath));
+						// 저장 경로에 바이트 타입의 이미지 저장
+						out.write(bytes);
+						
+						//클라이언트에 이벤트 추가
+						printWriter = response.getWriter();
+						response.setContentType("text/html");
+						
+						//파일이 연결되는 Url 주소 설정
+						String fileUrl = request.getContextPath() + "/images/boardimages/" + fileName;
+						
+						//생성된 jason 객체를 이용해 파일 업로드 + 이름 + 주소를 CkEditor에 전송
+						json.addProperty("uploaded", 1);
+						json.addProperty("fileName", fileName);
+						json.addProperty("url", fileUrl);
+						printWriter.println(json);
+					} catch (IOException e) {
+						e.printStackTrace();
+					} finally {
+						if(out !=null) {
+							out.close();
+						}
+						if(printWriter != null) {
+							printWriter.close();
+						}
+					}
+				}
+			}
+		}
+			return null;
+	}
+
+}
