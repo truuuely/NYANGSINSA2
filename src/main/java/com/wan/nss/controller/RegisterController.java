caa899f272b1cbc47a2478eb490904e606cbc701
kotddari <kotddari@gmail.com>
2023-03-10 10:12:39 +0900

    register

--- src/main/java/com/wan/nss/controller/RegisterController.java
+++ src/main/java/com/wan/nss/controller/RegisterController.java
@@ -10,12 +10,25 @@ import com.wan.nss.biz.member.MemberVO;
 
 @Controller
 public class RegisterController {
+	
 	@Autowired
 	private MemberService memberService;
+	@Autowired
+	private SmsService smsService;
 	
+<<<<<<< Updated upstream
 	@RequestMapping(value="/checkDuplication.do", method=RequestMethod.POST)
 	public String isDuplicated(MemberVO mvo) {
 		
+=======
+	// 아이디, 전화번호 중복 확인
+	@ResponseBody
+	@RequestMapping(value="/checkDuplication.do", method=RequestMethod.POST)
+	public String isDuplicated(MemberVO mvo) {
+		
+		System.out.println("checkDuplication.do 진입");
+		
+>>>>>>> Stashed changes
 		if(memberService.selectOne(mvo) == null) {
 			// 중복 없음 !
 			// 아이디 만들 수 있음
@@ -26,5 +39,44 @@ public class RegisterController {
 		} else {
 			return "0";
 		}
+		
 	}
+	
+	// 회원가입시 문자 인증 번호 발송
+	@RequestMapping(value="/sms.do")
+	public String sendSms(MemberVO mvo) {
+		
+		System.out.println("sms.do 진입");
+		
+		System.out.println(svo.getUserPhone()); //사용자 전화번호 가져오기
+		
+		int randNum = smsService.sns(svo);  //sns메서드 통해 randNum값 리턴받아옴.
+		System.out.println(randNum);
+		
+		//받아온 randNum값을 다시 ajax으로 보낸다 ->  success: function(randNum)  
+		return randNum+"";
+	}
+	
+	// 회원가입시 문자 인증 번호 확인
+	@RequestMapping(value="/checkSms.do")
+	public String checkSms(SmsVO svo) {
+		
+		System.out.println("checkSms.do 진입");
+		
+		System.out.println("로그:" + svo.getRandNum());
+		System.out.println("로그:" + svo.getChecknum());
+
+		if (svo.getRandNum().equals(svo.getChecknum())) { // 인증번호와 사용자입력값 비교
+
+			return "1"; // => result == "1" 을 뜻함
+			System.out.println("인증번호 일치함");
+			
+		} else {
+			
+			return "-1"; // => result == "-1" 을 뜻함
+			System.out.println("인증번호 불일치");
+		}
+		
+	}
+	
 }
