4b7de27845ce9df44d6e774c3d9e63f45ecd6890
kotddari <kotddari@gmail.com>
2023-03-13 10:21:05 +0900

    Update SmsServiceImpl.java

--- src/main/java/com/wan/nss/biz/sms/SmsServiceImpl.java
+++ src/main/java/com/wan/nss/biz/sms/SmsServiceImpl.java
@@ -3,7 +3,7 @@ package com.wan.nss.biz.sms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("reviewService")
+@Service("smsService")
 public class SmsServiceImpl implements SmsService {
 	
 	@Autowired
