ad4f533d444d7ceb0940e66e41c1f33fca4cafee
woojh7142 <woojh7142@gmail.com>
2023-03-15 14:10:05 +0900

    Update ReportService.java

--- src/main/java/com/wan/nss/biz/report/ReportService.java
+++ src/main/java/com/wan/nss/biz/report/ReportService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public interface ReportService {
 	public boolean insert(ReportVO rpvo);
+	//public ReportVO selectOne(ReportVO rvo);
 	public ArrayList<ReportVO> selectAll(ReportVO rpvo);
 	public boolean update(ReportVO rpvo);
 }
