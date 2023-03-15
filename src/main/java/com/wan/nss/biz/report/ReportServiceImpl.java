5d232b4a56f81062bb896ed330519115dbb61386
woojh7142 <woojh7142@gmail.com>
2023-03-15 14:10:08 +0900

    Update ReportServiceImpl.java

--- src/main/java/com/wan/nss/biz/report/ReportServiceImpl.java
+++ src/main/java/com/wan/nss/biz/report/ReportServiceImpl.java
@@ -12,13 +12,18 @@ public class ReportServiceImpl implements ReportService {
 	private ReportDAO reportDAO;
 	
 	@Override
+	public boolean insert(ReportVO rpvo) {
+		return reportDAO.insert(rpvo);
+	}
+	
+//	@Override
+//	public ReportVO selectOne(ReportVO rpvo) {
+//		return reportDAO.selectOne(rpvo);
+//	}
+	
+	@Override
 	public ArrayList<ReportVO> selectAll(ReportVO rpvo){
 		return reportDAO.selectAll(rpvo);
-	}
-
-	@Override
-	public ReportVO selectOne(ReportVO rpvo) {
-		return reportDAO.selectOne(rpvo);
 	}
 
 	@Override
