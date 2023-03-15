2a8e2a07c9e1ff869f56cc78654c288560d5358e
kotddari <kotddari@gmail.com>
2023-03-15 11:52:46 +0900

    Update ImageService.java

--- src/main/java/com/wan/nss/biz/image/ImageService.java
+++ src/main/java/com/wan/nss/biz/image/ImageService.java
@@ -6,6 +6,6 @@ public interface ImageService {
 	public ArrayList<ImageVO> selectAll(ImageVO pvo);
 	public ImageVO selectOne(ImageVO pvo);
 	public boolean insert(ImageVO pvo);
-	public boolean update(ImageVO pvo);
+//	public boolean update(ImageVO pvo);
 	public boolean delete(ImageVO pvo);
 }
